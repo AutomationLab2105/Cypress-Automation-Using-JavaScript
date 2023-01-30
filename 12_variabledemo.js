3979c0058b2857ccf6633bb4cea949cd86ecb996
AutomationLab2105 <123920074+AutomationLab2105@users.noreply.github.com>
2023-01-30 09:42:38 +0530

    Add files via upload

---
+++ 12_variabledemo.js
@@ -0,0 +1,16 @@
+//let var const
+
+let v1=100
+console.log(v1);
+v1=200
+console.log(v1);
+
+const v2=100
+console.log(v2);
+v2=200
+console.log(v2);
+
+/*case sensitive
+* Starting letter can _ or $
+*recommended to follow camalcase format -->phoneNumber
+*/
---
+++ 14_operators.js
@@ -0,0 +1,51 @@
+// let num1="3"
+// let num2="30"
+// let num3=num1+num2
+// console.log(num3); //330
+
+// let num1=3
+// let num2="30"
+// let num3=num1+num2
+// console.log(num3); //330
+
+// let num1="3"
+// let num2=30
+// let num3=num1+num2
+// console.log(num3); //330
+
+// let n1=3+30+300+'Cypress'
+// console.log(n1); //333Cypress
+// let n2="Cypress"+3+30+300
+// console.log(n2); //Cypress330300
+
+// let x1='4'-false
+// console.log(x1); //4
+// let x2='4'+false
+// console.log(x2); //4false
+
+
+
+// let x1='4'-true
+// console.log(x1); //3
+// let x2='4'+true
+// console.log(x2); //4true
+
+
+// let num4="33"
+// let num5="3"
+// let num6=num4-num5
+// console.log(num6);//30
+
+
+// let num4="33"
+// let num5="3"
+// let num6=num4+num5
+// console.log(num6);//333
+
+// let y1="Cypress"
+// let y2="Automation"
+// console.log("Output is ",y1-y2); // NaN
+
+let y1="Cypress"
+let y2="Automation"
+console.log("Output is ",y1+y2); //CypressAutomation
