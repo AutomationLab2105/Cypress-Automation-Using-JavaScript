435d47f187eac2fce8290aee8a4aa9dfb179e966
Chouda Reddy <123920074+AutomationLab2105@users.noreply.github.com>
2023-02-09 18:01:17 +0530

    Add files via upload

---
+++ Cypress-5/fixturedemo.cy.js
@@ -0,0 +1,13 @@
+describe('Fixture demo',function() {
+
+    beforeEach(function() {
+        cy.fixture("cypressdata").then(function(jsondata){
+            this.jsondata=jsondata
+        })
+    })
+    it('Fixture demo for JSON file',function(){
+        cy.log(this.jsondata.name)
+        cy.log(this.jsondata.email)
+        cy.log(this.jsondata.body)
+    })
+})
